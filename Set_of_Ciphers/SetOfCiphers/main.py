bfd35ef8d5437803147b7ac8b91152bd0a6fc770
Julio C. Oliveira <103333573+Julio-C-Oliveira@users.noreply.github.com>
2024-10-31 22:03:32 -0300

    update

---
+++ Set_of_Ciphers/SetOfCiphers/main.py
@@ -0,0 +1,6 @@
+import CesarCipher as Cesar, Replacement3x4 as Replace
+
+primary_word = input("Insira a palavra principal: ")
+secondary_word = input("Insira a palavra secundária: ")
+
+print(Cesar.CesarCipher(primaryWord=primary_word, secondaryWord=secondary_word, choice))
